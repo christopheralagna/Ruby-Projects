e40dcbfd9692e5abf01ebcb69a386ac8d8567160
Christopher Alagna <christopheralagna1@gmail.com>
2020-08-24 11:44:42 -0700

    in the middle of adding updates to include new game type

--- mastermind.rb
+++ mastermind.rb
@@ -2,26 +2,43 @@
 require 'pry'
 
 class Mastermind
-  attr_accessor :current_number_of_guesses, :allowed_number_of_guesses, :possible_colors, :colors_selected_by_computer, :correct_guesses, :colors_in_wrong_position, :remaining_colors
+  attr_accessor :current_number_of_guesses, :allowed_number_of_guesses, :possible_colors, :colors_selected_by_computer, :correct_guesses, :colors_in_wrong_position, :remaining_colors, :game_types
   @@current_number_of_guesses = 0
 
   def initialize(allowed_number_of_guesses)
     @allowed_number_of_guesses = allowed_number_of_guesses
-    @possible_colors = ['red', 'blue', 'yellow', 'green']
+    @possible_colors = ['red', 'blue', 'yellow', 'green', 'orange', 'pink']
     @colors_selected_by_computer = []
-    @correct_guesses = ['_', '_', '_', '_']
+    @correct_guesses = Array.new(@possible_colors.length, '_')
     @colors_in_wrong_position = 0
     @remaining_colors = []
     @player1 = Player.new()
+    @game_types = ["guesser", "setter"]
   end
 
-  def welcome()
-    puts "\n\n\nWelcome to Mastermind!" 
-    sleep(1)
+  def choose_game_type()
+    puts "\n\n\nWelcome to Mastermind!"
+    puts "\nWould you prefer to be the guesser or the master?"
+    response = gets.chomp()
+    until @game_types.any? == response
+      puts "\nPlease select a valid game type..."
+      response = gets.chomp()
+    end
+    if response == "guesser"
+      return "guesser"
+    elsif response == "setter"
+      return "setter"
+    end
+  end
+
+  def welcome_game_type_one()
     puts "\nIn this instance, you will be the guesser, and the computer will be the creator"
     sleep(3)
-    puts "\nYou have #{@allowed_number_of_guesses} tries to guess the secret color code!"
+    puts "\nThere are #{@possible_colors.length} possible colors, and #{@possible_colors.length} colors to guess in the right order"
+    puts "#{@correct_guesses.join(' ')}   <-- 'O' represents a correct guess"
     sleep(3)
+    puts "\nYou have #{@allowed_number_of_guesses} tries to guess the secret color code!"
+    new_round()
   end
 
   def new_round()
@@ -42,7 +59,7 @@ class Mastermind
   end
 
   def guess_colors()
-    4.times do
+    @possible_colors.length.times do
       puts "\nPlease select a color..."
       selection = gets.chomp
       until @possible_colors.any? { |color| color == selection }
@@ -107,7 +124,7 @@ class Mastermind
       winner(false)
     else
       puts "\nYou have #{@allowed_number_of_guesses - @@current_number_of_guesses} guesses left..."
-      @correct_guesses = ['_', '_', '_', '_']
+      @correct_guesses = Array.new(@possible_colors.length, '_')
       sleep(1)
       new_round()
     end
@@ -137,19 +154,58 @@ attr_accessor :possible_colors
 
   def initialize(possible_colors)
     @possible_colors = possible_colors
+    @new_guesses = Array.new(@possible_colors.length, '_')
   end
 
   def random_color_select() 
     random_colors = []
-    4.times do
-      random_colors.push(@possible_colors[rand(4)])
+    @possible_colors.length.times do
+      random_colors.push(@possible_colors[rand(@possible_colors.length)])
     end
     return random_colors
   end
 
+  def get_random_color()
+    return @possible_colors[rand(@possible_colors.length)]
+  end
+
+  def get_computer_guesses()
+    @new_guesses.each_with_index do |guess, i|
+      if @correct_guesses[i] == 'O'
+        guess = 'O'
+      elsif @correct_guesses[i] == '_'
+        guess = get_random_color()
+      end
+    return @new_guesses
+  end
+
 end
 
-new_game = Mastermind.new(1)
-new_game.welcome
-new_game.get_computer_colors
-new_game.new_round
+module GameTypeComputerGuess
+  
+end
+
+new_game = Mastermind.new(12)
+game_type = new_game.choose_game_type
+if game_type == "guesser"
+  new_game.get_computer_colors
+  new_game.welcome_game_type_one
+elsif game_type == "setter"
+
+end
+
+#MasterMind method - def choose_game_type --> choose guesser or master
+#computer variable --> @new_guesses = Array.new(@possible_colors.length, "_")
+
+#Computer method - get_random_color() --> creates a random color
+
+#Computer method - random_color_guess()
+
+  #iterates through @new_guesses.each_with_index |guess, i|
+  #if @correct_guesses[i] == 'O'
+    #guess = 'O'
+  #elsif @correct_guesses[i] == '_'
+    #guess = get_random_color()
+  #return array like "[red, "O", blue, "O"]" where the colors listed are the new random guesses
+
+#
