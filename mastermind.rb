8697fa5515e95653b4322064b32c7f1d9a99f49c
Christopher Alagna <christopheralagna1@gmail.com>
2020-08-25 13:13:48 -0700

    all routing for the game works with both modes; established a module for SharedGameMethods; need to fix get_guesses for ComputerGuesser class

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,104 @@
 
+=begin
+
+CONTENTS...
+
+module SharedGameMethods; end
+
+module Mastermind
+  class GameType1; end
+  class GameType2; end
+end
+
+module Player
+  class PlayerGuesser; end
+  class PlayerSetter; end
+end
+
+module Computer
+  class ComputerSetter; end
+  class ComputerGuesser; end
+end
+
+=end
+
 require 'pry'
+
+module SharedGameMethods
+
+  def new_round(guesser, setter_selection)
+    guessed_colors = guesser.get_guesses(@possible_colors)
+    determine_guess_accuracy(guessed_colors, setter_selection)
+    update_score(guesser)
+    unless @correct_guesses.all? { |guess| guess == 'O' }
+      update_guess_count()
+      if @player1.role == "guesser"
+        return_hints(guessed_colors)
+      end
+    end
+    game_status(guesser, setter_selection)
+  end
+
+  def determine_guess_accuracy(guessed_colors, colors_selected_by_setter) 
+    guessed_colors.each_with_index do |color, i| 
+      if color == colors_selected_by_setter[i]
+        @correct_guesses[i] = 'O'
+      else
+        @remaining_colors.push(colors_selected_by_setter[i])
+      end
+    end
+    puts "\nHere are the results for this round:"
+    puts "#{@correct_guesses.join(' ')}   <-- 'O' represents a correct guess\n\n"
+    sleep(2)
+  end
+
+  def update_score(guesser) 
+    guesser.number_of_correct_guesses =  @correct_guesses.reduce(0) do |score, guess|
+      if guess == 'O'
+        score + 1
+      else score
+      end
+    end
+  end
+
+  def update_guess_count()
+    @current_number_of_guesses += 1
+  end
+
+  def game_status(guesser, setter_selection)
+    if @current_number_of_guesses == @allowed_number_of_guesses && @player1.role == "guesser"
+      puts "\nYou are out of guesses..."
+      winner(false)
+    elsif @current_number_of_guesses == @allowed_number_of_guesses && @player1.role == "setter"
+      puts "\nThe computer is out of guesses..."
+      winner(true)
+    elsif @correct_guesses.all? { |guess| guess == 'O'} && @player1.role == "guesser"
+      puts "\nYou have guessed all colors correctly within #{@allowed_number_of_guesses} attempts..."
+      winner(true)
+    elsif @correct_guesses.all? { |guess| guess == 'O'} && @player1.role == "setter"
+      puts "\nThe computer has guessed all colors correctly within #{@allowed_number_of_guesses} attempts..."
+      winner(false)
+    else
+      puts "\nThere are #{@allowed_number_of_guesses - @current_number_of_guesses} guesses left..."
+      @correct_guesses = Array.new(@possible_colors.length, '_')
+      sleep(1)
+      new_round(guesser, setter_selection)
+    end
+  end
+
+  def winner(outcome)
+    puts outcome ? "\nYou Win!\n" : "\nYou Lose!\n"
+  end
+
+end
 
 module Mastermind
   class GameType1
-  attr_accessor :current_number_of_guesses, :allowed_number_of_guesses, :possible_colors, :colors_selected_by_computer, :correct_guesses, :colors_in_wrong_position, :remaining_colors, :game_types
-  @@current_number_of_guesses = 0
+  include SharedGameMethods
+  attr_accessor :current_number_of_guesses, :allowed_number_of_guesses, :possible_colors, :colors_selected_by_computer, :correct_guesses, :colors_in_wrong_position, :remaining_colors, :player1, :computer
 
     def initialize(allowed_number_of_guesses, possible_colors)
+      @current_number_of_guesses = 0
       @allowed_number_of_guesses = allowed_number_of_guesses
       @possible_colors = possible_colors
       @colors_selected_by_computer = []
@@ -14,7 +106,7 @@ module Mastermind
       @colors_in_wrong_position = 0
       @remaining_colors = []
       @player1 = Player::PlayerGuesser.new()
-      @game_types = ["guesser", "setter"]
+      @computer = Computer::ComputerSetter.new()
     end
 
     def start_game_type_one()
@@ -25,72 +117,14 @@ module Mastermind
       puts "#{@correct_guesses.join(' ')}   <-- 'O' represents a correct guess"
       sleep(3)
       puts "\nYou have #{@allowed_number_of_guesses} tries to guess the secret color code!"
-      new_round()
-    end
-  
-    def new_round()
-      guessed_colors = guess_colors()
-      determine_guess_accuracy(guessed_colors)
-      update_score()
-      unless @correct_guesses.all? { |guess| guess == 'O' }
-        update_guess_count()
-        return_hints(guessed_colors)
-        game_status()
-      else winner(true)
-      end
-    end
-    
-    def get_computer_colors() 
-      computer = Computer::ComputerSetter.new(@possible_colors)
-      @colors_selected_by_computer = computer.random_color_select()
-    end
-  
-    def guess_colors()
-      @possible_colors.length.times do
-        puts "\nPlease select a color..."
-        selection = gets.chomp
-        until @possible_colors.any? { |color| color == selection }
-          puts "\nPlease select a VALID color"
-          selection = gets.chomp
-        end
-        @player1.guessed_colors.push(selection)
-      end
-      guessed_colors = @player1.guessed_colors
-      puts "\nHere are the colors you guessed: [#{guessed_colors.join(' ')}]"
-      sleep(3)
-      return guessed_colors
-    end
-  
-    def determine_guess_accuracy(guessed_colors) 
-      guessed_colors.each_with_index do |color, i| 
-        if color == @colors_selected_by_computer[i]
-          @correct_guesses[i] = 'O'
-        else
-          @remaining_colors.push(@colors_selected_by_computer[i])
-        end
-      end
-      puts "\nHere are your results for this round:"
-      puts "#{@correct_guesses.join(' ')}   <-- 'O' represents a correct guess\n\n"
-      sleep(3)
-    end
-    
-    def update_score() 
-      @player1.number_of_correct_guesses =  @correct_guesses.reduce(0) do |score, guess|
-        if guess == 'O'
-          score + 1
-        else score
-        end
-      end
-    end
-  
-    def update_guess_count()
-      @@current_number_of_guesses += 1
+      @colors_selected_by_computer = @computer.set_computer_colors(@possible_colors)
+      new_round(@player1, @colors_selected_by_computer)
     end
   
     def return_hints(guessed_colors)
       puts "Here are your following hints..."
       sleep(2)
-      correct_guesses.each_with_index do |guess, i|
+      @correct_guesses.each_with_index do |guess, i|
         if guess == "_" && @remaining_colors.include?(@player1.guessed_colors[i])
           puts "\tYour guess of Position #{i + 1} (#{@player1.guessed_colors[i]}) is in the wrong position"
           @colors_in_wrong_position += 1
@@ -104,41 +138,23 @@ module Mastermind
       @remaining_colors = []
       sleep(2)
     end
-  
-    def game_status()
-      if @@current_number_of_guesses == @allowed_number_of_guesses 
-        winner(false)
-      else
-        puts "\nYou have #{@allowed_number_of_guesses - @@current_number_of_guesses} guesses left..."
-        @correct_guesses = Array.new(@possible_colors.length, '_')
-        sleep(1)
-        new_round()
-      end
-    end
-  
-    def winner(outcome)
-      puts outcome ? "You Win!" : "\nYou are out of tries! You Lose!\n"
-    end
+
   end
 
   class GameType2
-  attr_accessor :current_number_of_guesses, :allowed_number_of_guesses, :possible_colors, :colors_selected_by_player, :correct_guesses, :colors_in_wrong_position, :remaining_colors, :game_types
-  @@current_number_of_guesses = 0
+  include SharedGameMethods
+  attr_accessor :current_number_of_guesses, :allowed_number_of_guesses, :possible_colors, :colors_selected_by_player, :correct_guesses, :colors_in_wrong_position, :remaining_colors, :player1, :computer
 
     def initialize(allowed_number_of_guesses, possible_colors)
+      @current_number_of_guesses = 0
       @allowed_number_of_guesses = allowed_number_of_guesses
       @possible_colors = possible_colors
       @colors_selected_by_player = []
       @correct_guesses = Array.new(@possible_colors.length, '_')
       @colors_in_wrong_position = 0
       @remaining_colors = []
-      @computer = Computer::ComputerGuesser.new(possible_colors)
-      @game_types = ["guesser", "setter"]
-    end
-
-    def get_player_colors()
-      player = Player::PlayerSetter.new()
-      @colors_selected_by_player = player.select_player_colors(@possible_colors)
+      @player1 = Player::PlayerSetter.new()
+      @computer = Computer::ComputerGuesser.new()
     end
 
     def start_game_type_two()
@@ -148,34 +164,51 @@ module Mastermind
       puts "You will set #{@possible_colors.length} colors in a particular order, which the computer will have to guess!"
       sleep(3)
       puts "\nThe computer will have #{@allowed_number_of_guesses} tries to guess the secret color code!"
-      new_round()
+      @colors_selected_by_player = @player1.set_player_colors(@possible_colors)
+      new_round(@computer, @colors_selected_by_player)
     end
 
-    def new_round()
-      
-    end
   end
 end
 
 module Player
   class PlayerGuesser
-  attr_accessor :number_of_correct_guesses, :guessed_colors, :incorrect_guesses
+  attr_accessor :number_of_correct_guesses, :guessed_colors, :incorrect_guesses, :role
 
     def initialize()
       @number_of_correct_guesses = 0
       @guessed_colors = []
       @incorrect_guesses = []
+      @role = "guesser"
     end
+
+    def get_guesses(possible_colors)
+      puts "\nSelect #{possible_colors.length} colors, one at a time..."
+      possible_colors.length.times do
+        puts "\nPlease select a color..."
+        selection = gets.chomp
+        until possible_colors.any? { |color| color == selection }
+          puts "\nPlease select a VALID color"
+          selection = gets.chomp
+        end
+        @guessed_colors.push(selection)
+      end
+      puts "\nHere are the colors you guessed: [#{@guessed_colors.join(' ')}]"
+      sleep(3)
+      return @guessed_colors
+    end
+
   end
 
   class PlayerSetter
-  attr_accessor :set_colors
+  attr_accessor :set_colors, :role
 
     def initialize()
       @set_colors = []
+      role = "setter"
     end
 
-    def select_player_colors(possible_colors)
+    def set_player_colors(possible_colors)
       puts "Select #{possible_colors.length} colors, one at a time..."
       possible_colors.length.times do
         puts "\nPlease select a color..."
@@ -195,44 +228,46 @@ end
 
 module Computer
   class ComputerSetter
-  attr_accessor :possible_colors
+  attr_accessor :set_colors
 
-    def initialize(possible_colors)
-      @possible_colors = possible_colors
+    def initialize()
+      @set_colors = []
     end
 
-    def random_color_select() 
-      random_colors = []
-      @possible_colors.length.times do
-        random_colors.push(@possible_colors[rand(@possible_colors.length)])
+    def set_computer_colors(possible_colors) 
+      possible_colors.length.times do
+        @set_colors.push(possible_colors[rand(possible_colors.length)])
       end
-      return random_colors
+      return @set_colors
     end
 
   end
 
   class ComputerGuesser
-  attr_accessor :possible_colors, :new_guesses
+  attr_accessor :number_of_correct_guesses, :guessed_colors, :incorrect_guesses
 
-    def initialize(possible_colors)
-      @possible_colors = possible_colors
-      @new_guesses = Array.new(@possible_colors.length, '_')
+    def initialize()
+      @number_of_correct_guesses = 0
+      @guessed_colors = []
+      @incorrect_guesses = []
+    end
+
+    def get_guesses(correct_guesses)
+      puts "The computer is thinking..."
+      sleep(2)
+      correct_guesses.each_with_index do |guess, i|
+        if guess == 'O'
+          @guessed_colors.push('O')
+        elsif guess == '_'
+          @guessed_colors.push(get_random_color())
+        end
+      end
     end
 
     def get_random_color()
       return @possible_colors[rand(@possible_colors.length)]
     end
 
-    def get_computer_guesses()
-      @new_guesses.each_with_index do |guess, i|
-        if @correct_guesses[i] == 'O'
-          guess = 'O'
-        elsif @correct_guesses[i] == '_'
-          guess = get_random_color()
-        end
-      end
-      return @new_guesses
-    end
   end
 end
 
@@ -252,14 +287,13 @@ def choose_game_type()
 end
 
 possible_colors = ['red', 'blue', 'yellow', 'green', 'orange', 'pink']
+allowed_number_of_guesses = 5
 
 game_type = choose_game_type()
 if game_type == "guesser"
-  new_game = Mastermind::GameType1.new(12, possible_colors)
-  new_game.get_computer_colors
-  new_game.start_game_type_one
+  new_game = Mastermind::GameType1.new(allowed_number_of_guesses, possible_colors)
+  new_game.start_game_type_one()
 elsif game_type == "setter"
-  new_game = Mastermind::GameType2.new(12, possible_colors)
-  new_game.get_player_colors
-  new_game.start_game_type_two
+  new_game = Mastermind::GameType2.new(allowed_number_of_guesses, possible_colors)
+  new_game.start_game_type_two()
 end
